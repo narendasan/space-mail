b2dc76baf74525d1c049758a883dd05be6ec3922
Tri Irfan <irfant@twitter.com>
2015-08-08 21:43:33 -0700

    Improve UI

--- README.md
+++ README.md
@@ -1,18 +1,4 @@
-Example app for GMail package
+Mail Filtering App
 ===
 
-Create a new Google Application, set the JS Origin as `http://localhost:3000`
-and callback URI as `http://localhost:3000/_oauth/google`.
-
-Also add the GMail API to the list of APIs.
-
-Set up the Google Accounts configuration:
-
-    ServiceConfiguration.configurations.insert({service:"google", "clientId" : <ClientID>, "secret" : <YourSecret>, "loginStyle" : "popup"})
-
-Login with your Google account on the front-page.
-
-All messages matching "google i/o" will be printed to the server console.
-
-
-
+Meteor Student Summer Hackathon 2015
--- client/base.js
+++ client/base.js
@@ -1,40 +1,14 @@
-var email_data =
-  [
-    {
-      id: 1,
-      time: "June 21, 2015. 10:45am",
-      from: "Matthew Miner",
-      subject: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-      content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, aspernatur, quos! Omnis et odio eos dolorum quibusdam quam, assumenda soluta."
-    },
-    {
-      id: 2,
-      time: "June 21, 2015. 10:45am",
-      from: "Matthew Miner",
-      subject: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-      content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, aspernatur, quos! Omnis et odio eos dolorum quibusdam quam, assumenda soluta."
-    },
-    {
-      id: 3,
-      time: "June 21, 2015. 10:45am",
-      from: "Matthew Miner",
-      subject: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-      content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, aspernatur, quos! Omnis et odio eos dolorum quibusdam quam, assumenda soluta."
-    },
-    {
-      id: 4,
-      time: "June 21, 2015. 10:45am",
-      from: "Matthew Miner",
-      subject: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-      content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, aspernatur, quos! Omnis et odio eos dolorum quibusdam quam, assumenda soluta."
-    }
-  ]
-
 Template.collection.helpers({
   email_data: function () {
     return Emails.find();
   }
 });
+
+Template.notification.helpers({
+  tags: function() {
+    return ["Github", "LinkedIn", "Facebook", "Twitter"]
+  }
+})
 
 Accounts.ui.config({
   requestOfflineToken: { google: true },
