771b7cb0d40f3faf32b4a3497e9d5f26ee3d10fd
Naren <naren@narendasan.com>
2015-08-09 10:04:07 -0700

    alphabetical

--- client/base.js
+++ client/base.js
@@ -23,9 +23,21 @@ Template.full.helpers({
 
 Template.notification.helpers({
   tags: function() {
-    return Tags.find({uid: Meteor.user().services.google.id});
+    return Tags.find({uid: Meteor.user().services.google.id}, {sort: {name: 1}});
   }
 });
+
+Array.prototype.move = function (old_index, new_index) {
+    if (new_index >= this.length) {
+        var k = new_index - this.length;
+        while ((k--) + 1) {
+            this.push(undefined);
+        }
+    }
+    this.splice(new_index, 0, this.splice(old_index, 1)[0]);
+    return this; // for testing purposes
+};
+
 
 Accounts.ui.config({
   requestOfflineToken: { google: true },
@@ -33,4 +45,50 @@ Accounts.ui.config({
   requestPermissions: { google: ["https://www.googleapis.com/auth/gmail.readonly"] }
 });
 
-Template.send
+Schemas = {};
+Template.registerHelper("Schemas", Schemas);
+
+Schemas.outboundMail = new SimpleSchema({
+    subject: {
+      type: String,
+      label: "Subject",
+      optional: true,
+      max: 200
+    },
+    to: {
+      type: String,
+      label: "To"
+    },
+    cc: {
+      type: String,
+      label: "CC",
+      optional: true,
+      min: 0
+    },
+    bcc: {
+      type: String,
+      label: "BCC",
+      optional: true
+    },
+    body: {
+      type: String,
+      label: "",
+      optional: true,
+      max: 1000
+    }
+});
+
+Collections = {};
+Template.registerHelper("Collections", Collections);
+Outbound = Outbound.Collections = new Mongo.Collection("outbound");
+Outbound.attachSchema(Schema.outboundMail);
+
+Meteor.publish(null, function () {
+  return Outbound.find();
+});
+
+Outbound.allow({
+  insert: function () {
+    return true;
+  }
+});
